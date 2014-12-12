5dc084b0da66a9e0c5b0b27db0d4f1001ed821c2
Robin <robin.hayart@gmail.com>
2014-12-13 00:36:01 +0100

    Ajout de l'activité paramètre ajout ou non des courbes modification du code original ajout de la courbe blink

--- src/com/example/testapp/MainActivity.java
+++ src/com/example/testapp/MainActivity.java
@@ -8,12 +8,14 @@ import android.app.AlertDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.util.Log;
 // Imports api GraphView
@@ -39,6 +41,10 @@ public class MainActivity extends Activity {
     double min = 0;
     
     int passage = 1;
+    
+    boolean courbeAttention = true;
+    boolean courbeMeditation = true;
+    boolean courbeBlink = true;
 	
     // Courbe de l'attention (Couleur = rouge / Nom = Attention)
     GraphViewSeries seriesAttention = new GraphViewSeries("Attention", new GraphViewSeriesStyle(Color.rgb(200, 50, 00), 3), new GraphViewData[] {
@@ -48,6 +54,14 @@ public class MainActivity extends Activity {
     GraphViewSeries seriesMeditation = new GraphViewSeries("Meditation", new GraphViewSeriesStyle(Color.rgb(0, 50, 200), 3), new GraphViewData[] {
 
     });
+    
+    // Courbe de la méditation (Couleur = bleu / Nom = Meditation)
+    GraphViewSeries seriesBlink = new GraphViewSeries("Clins d'oeil", new GraphViewSeriesStyle(Color.rgb(0, 200, 50), 3), new GraphViewData[] {
+
+    });
+    
+    // Instanciation du GraphView
+    GraphView graphView;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +78,45 @@ public class MainActivity extends Activity {
         createGraph();
 		
 	}
+    
+    protected void onResume(){
+    	super.onResume();
+    	
+    	gestionParametre();
+    }
+    
+    protected void gestionParametre(){
+    	try{
+	    	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+	    	courbeAttention = prefs.getBoolean("graph_attention", true);
+	    	courbeMeditation = prefs.getBoolean("graph_meditation", true);
+	    	courbeBlink = prefs.getBoolean("graph_blink", true);
+    	
+	    	if(!courbeAttention){
+	    		graphView.removeSeries(seriesAttention);
+	    	}else if(courbeAttention != prefs.getBoolean("graph_attention", true)){
+	    		graphView.addSeries(seriesAttention);
+	    	}
+	    	
+	    	if(!courbeMeditation){
+	    		graphView.removeSeries(seriesMeditation);
+	    	}else if(courbeMeditation != prefs.getBoolean("graph_meditation", true)){
+	    		graphView.addSeries(seriesMeditation);
+	    	}
+	    	
+	    	if(!courbeBlink){
+	    		graphView.removeSeries(seriesBlink);
+	    	}else if(courbeBlink != prefs.getBoolean("graph_blink", true)){
+	    		graphView.addSeries(seriesBlink);
+	    	}
+	    	
+	    	if(!courbeAttention && !courbeBlink && !courbeMeditation){
+	    		Toast.makeText(getApplicationContext(), "Aucune courbe sélectionné, sélectionnez en une dans les paramètres", Toast.LENGTH_LONG).show();
+	    	}
+    	}catch(Exception exc){
+    		Log.e("errorResume", "Erreur : " +exc);
+    	}
+    }
         
     // Handler du ThinkGear Device (thread qui traite constamment les données reçus)
     @SuppressLint("HandlerLeak") private final Handler handler = new Handler() {
@@ -76,11 +129,11 @@ public class MainActivity extends Activity {
     					case TGDevice.STATE_IDLE:
     					break;
     					case TGDevice.STATE_CONNECTING:
-    						Log.v("Test1", "Connecting");
+    						Log.v("Statut", "Connecting");
     						Toast.makeText(getApplicationContext(), "Connection en cours ...", Toast.LENGTH_SHORT).show();
 						break;
 						case TGDevice.STATE_CONNECTED:
-							Log.v("Test1", "Connected");
+							Log.v("Statut", "Connected");
 							Toast.makeText(getApplicationContext(), "Connecté !", Toast.LENGTH_SHORT).show();
 							tgDevice.start();
 						break;
@@ -91,26 +144,26 @@ public class MainActivity extends Activity {
 							Toast.makeText(getApplicationContext(), "Systeme non trouvé !", Toast.LENGTH_SHORT).show();
 						break;
 						case TGDevice.STATE_NOT_PAIRED:
-							Log.v("Test1", "Not paired");
+							Log.v("Statut", "Not paired");
 							tgDevice.connect(true);
 						default:
 						break;
     				}
     			break;
     			case TGDevice.MSG_POOR_SIGNAL: // Indique la qualité du signal (0 indique un bon fonctionnement)
-    				Log.v("HelloEEG", "PoorSignal: " + msg.arg1);
+    				Log.v("MsgEEG", "PoorSignal: " + msg.arg1);
     			break;
     			case TGDevice.MSG_ATTENTION:
     				/*
     				 * Renvoie une valeur entre 0 et 100 sur la capacité d'attention
     				 * 0 : incapacité à calculé une valeur d'attention
-    				 * 1 - 20 : faible attention
-    				 * 20 - 40 : peu d'attention
-    				 * 40 - 50 : valeur d'attention normal
-    				 * 50 - 80 : attention relativement élevé
+    				 * 1 - 20 : tres faible attention
+    				 * 20 - 40 : faible attention
+    				 * 40 - 60 : valeur d'attention normal
+    				 * 60 - 80 : attention relativement élevé
     				 * 80 - 100 : attention élevé
     				 */
-    				Log.v("HelloEEG", "Attention: " + msg.arg1);
+    				Log.v("MsgEEG", "Attention: " + msg.arg1);
     				  seriesAttention.appendData( new GraphViewData(passage, msg.arg1), true); 
     				  passage++;
     			break;
@@ -118,40 +171,42 @@ public class MainActivity extends Activity {
     				/*
     				 * Renvoie une valeur entre 0 et 100 sur la capacité de méditation
     				 * 0 : incapacité à calculé une valeur de meditation
-    				 * 1 - 20 : faible meditation
-    				 * 20 - 40 : peu de de meditation
-    				 * 40 - 50 : valeur de meditation normal
-    				 * 50 - 80 : meditation relativement élevé
+    				 * 1 - 20 : tres faible meditation
+    				 * 20 - 40 : faible meditation
+    				 * 40 - 60 : valeur de meditation normal
+    				 * 60 - 80 : meditation relativement élevé
     				 * 80 - 100 : meditation élevé
     				 */
-    				Log.v("HelloEEG","Meditation: " +msg.arg1);
+    				Log.v("MsgEEG","Meditation: " +msg.arg1);
     				seriesMeditation.appendData( new GraphViewData(passage, msg.arg1), true); 
     			break;
     			case TGDevice.MSG_RAW_DATA:
     				int rawValue = msg.arg1;
-    				Log.v("HelloRawData", "Raw Data : " +rawValue);
+    				int test = msg.arg2;
+    				Log.v("MsgRawData", "Raw Data : " +rawValue + " / " +test);
     			break;
     			case TGDevice.MSG_HEART_RATE:
-    				Log.v("HelloEEG","Heart Rate " +msg.arg1);
+    				Log.v("MsgEEG","Heart Rate " +msg.arg1);
     			break;
     			case TGDevice.MSG_BLINK:
-    				Log.v("HelloEEG","Blink : " +msg.arg1);
+    				seriesBlink.appendData( new GraphViewData(passage, msg.arg1), true);
+    				Log.v("MsgEEG","Blink : " +msg.arg1);
     			break;
     			case TGDevice.MSG_SLEEP_STAGE:
-    				Log.v("HelloEEG", "Sleep stage : " +msg.arg1);
+    				Log.v("MsgEEG", "Sleep stage : " +msg.arg1);
     			break;
     			case TGDevice.MSG_RAW_MULTI:
                 	TGRawMulti rawM = (TGRawMulti)msg.obj;
-                	Log.v("HelloRawMulti","Raw1: " + rawM.ch1 + "\nRaw2: " + rawM.ch2);
+                	Log.v("MsgRawMulti","Raw1: " + rawM.ch1 + "\nRaw2: " + rawM.ch2);
     			break;
                 case TGDevice.MSG_LOW_BATTERY:
                 	Toast.makeText(getApplicationContext(), "Batterie faible !", Toast.LENGTH_SHORT).show();
                 break;
     			case TGDevice.MSG_EEG_POWER:
     				TGEegPower ep = (TGEegPower)msg.obj;
-    				Log.v("HelloEEGD", "Delta: " + ep.delta);
-    				Log.v("HelloEEGGL","Gamma Low : " + ep.lowGamma);
-    				Log.v("HelloEEGGM","Gamma Mid : " + ep.midGamma);
+    				Log.v("MsgEEGD", "Delta: " + ep.delta);
+    				Log.v("MsgEEGGL","Gamma Low : " + ep.lowGamma);
+    				Log.v("MsgEEGGM","Gamma Mid : " + ep.midGamma);
     			default:
     			break;
     		}
@@ -160,16 +215,14 @@ public class MainActivity extends Activity {
 
     // Création du graphique attention / meditation
     public void createGraph(){
-    	
-        // Instanciation du GraphView
-        GraphView graphView = new LineGraphView( 
-                this, 
-                "Activité"
-        );
 
+    	graphView = new LineGraphView(this, "Courbes EEG");
+    	
+    	graphView.addSeries(seriesMeditation);
+    	graphView.addSeries(seriesAttention);
+    	graphView.addSeries(seriesBlink);
+    	
 		graphView.setManualYAxisBounds((double) max, (double) min);
-		graphView.addSeries(seriesAttention);
-		graphView.addSeries(seriesMeditation);
 		graphView.setShowLegend(true);
 		graphView.setViewPort(1,25);
 		graphView.setScrollable(true);
@@ -193,11 +246,16 @@ public class MainActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch (item.getItemId()) {
+    	case R.id.settings:
+    		// Comportement du bouton "Paramètres"
+    		Intent settingsIntent = new Intent(this,SettingsActivity.class);
+    		startActivity(settingsIntent);
+    		return true;
     	case R.id.propos:
     		// Comportement du bouton "A propos"
     		new AlertDialog.Builder(this)
     	    .setTitle("A propos")
-    	    .setMessage("Application réalisé dans le cadre du projet BrainWaves de Licence Pro Orleans.\n" +
+    	    .setMessage("Application réalisé dans le cadre du projet BrainWaves de Licence Pro Dev Web et Mobile d'Orleans.\n" +
     	    		"- Robin Hayart \n" +
     	    		"- Loic Dieudonné \n" +
     	    		"- Chafik Daggag \n" +
