e25f0e10d3b037fcb025cff4463facc4fd1958be
DOMEY <dimitri.domey@gmail.com>
2015-03-24 14:33:31 +0100

    Action bar buton on compare graph

--- src/fr/iut/brainwave/CompareActivity.java
+++ src/fr/iut/brainwave/CompareActivity.java
@@ -2,6 +2,8 @@ package fr.iut.brainwave;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class CompareActivity extends Activity {
 	
@@ -9,5 +11,23 @@ public class CompareActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_compare);
+	}
+	
+    /**
+     * MANAGE action bar
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.compare_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			default: 
+	    		return super.onOptionsItemSelected(item);
+		}
 	}
 }
