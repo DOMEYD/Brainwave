591dfaf6fd52e0d9b31f66ece2b277755220fed5
Kasibear <benj-le-kiwi@hotmail.fr>
2015-03-25 21:00:31 +0100

    Add and Remove CSVFiles DialogBox Problem with graphic

--- src/fr/iut/brainwave/CompareActivity.java
+++ src/fr/iut/brainwave/CompareActivity.java
@@ -46,17 +46,27 @@ import android.widget.Toast;
 
 public class CompareActivity extends Activity {
 
-	private	Dialog d = null;
+	private	Dialog ad = null;
+	private	Dialog rd = null;
 	public ArrayList<File> ArrayListCsvFiles = new ArrayList<File>();
+	public ArrayList<File> ArrayFilesCharging = new ArrayList<File>();
 	private ListView LvAllCsvFiles;
-	private TextView TvFilesTitle;
+	private ListView LvFilesCharging;
+
 	public ArrayAdapter<String> AdaptateurFiles;
+	public ArrayAdapter<String> AdaptateurFilesCharging;
 	public ArrayList<Integer[]> ArrayDataImportFile = new ArrayList<Integer[]>();
+	public ArrayList<Integer[]> ArrayDataImportFile2 = new ArrayList<Integer[]>();
+	
 	
     // Courbe de l'attention (Couleur = rouge / Nom = Attention)
     GraphViewSeries seriesAttention = new GraphViewSeries("Attention", new GraphViewSeriesStyle(Color.rgb(200, 50, 00), 3), new GraphViewData[] {});
     // Courbe de la méditation (Couleur = bleu / Nom = Meditation)
     GraphViewSeries seriesMeditation = new GraphViewSeries("Meditation", new GraphViewSeriesStyle(Color.rgb(0, 50, 200), 3), new GraphViewData[] {});
+ // Courbe de l'attention (Couleur = rouge / Nom = Attention)
+    GraphViewSeries seriesAttention2 = new GraphViewSeries("Attention2", new GraphViewSeriesStyle(Color.rgb(200, 50, 00), 3), new GraphViewData[] {});
+    // Courbe de la méditation (Couleur = bleu / Nom = Meditation)
+    GraphViewSeries seriesMeditation2 = new GraphViewSeries("Meditation2", new GraphViewSeriesStyle(Color.rgb(0, 50, 200), 3), new GraphViewData[] {});
    
     // Instanciation du GraphView
     GraphView graphView;
@@ -78,38 +88,84 @@ public class CompareActivity extends Activity {
 		}   
 	}
 	
+	private void getListFilesCharging() {
 
+		AdaptateurFilesCharging = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+		LvFilesCharging.setAdapter(AdaptateurFilesCharging);
+
+		//SimpleDateFormat sdfDate = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.FRANCE);
+
+		for (File file :ArrayFilesCharging) {	
+			
+			AdaptateurFilesCharging.add(file.getName()+"\n"/*+ sdfDate.format(file.lastModified()) */);
+		}   
+
+	}
 	
-	private void FilesBox(){
-			d = new Dialog(CompareActivity.this);
-	        d.setTitle(getString(R.string.paramsFilesDialogTitle));
-	        d.setContentView(R.layout.dialog_list);
-	        Button b1 = (Button) d.findViewById(R.id.button_validation);
-	        Button b2 = (Button) d.findViewById(R.id.button_cancel);
+	private void RemoveFilesBox(){
+		
+			rd = new Dialog(CompareActivity.this);
+	        rd.setTitle(getString(R.string.paramsFilesDialogTitle));
+	        rd.setContentView(R.layout.dialog_remove_list);
+	        Button b1 = (Button) rd.findViewById(R.id.button_validation);
+	        Button b2 = (Button) rd.findViewById(R.id.button_cancel);
 
 			//Titres des listes
-			AdaptateurFiles = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-			
-			LvAllCsvFiles = (ListView) d.findViewById(R.id.lvCSVFiles);	
-			LvAllCsvFiles.setOnItemClickListener(mCSVClickListener);
-			        
-			getListFiles();
+	        AdaptateurFilesCharging = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2);
+
+			LvFilesCharging = (ListView) rd.findViewById(R.id.lvCSVFiles);	
+			LvFilesCharging.setOnItemClickListener(mCSVClickRemoveListener);
+
+			getListFilesCharging();
 			
 	        b1.setOnClickListener(new OnClickListener() {
 		         @Override
 		         public void onClick(View v) {
-		        	 d.dismiss();  
+		        	 rd.dismiss();  
 		          }    
 	        });
 	        b2.setOnClickListener(new OnClickListener() {
 		         @Override
 		         public void onClick(View v) {
-		            d.dismiss(); // dismiss the dialog
+		            rd.dismiss(); // dismiss the dialog
 		         }    
 	        });
 	        
-	        d.show();
+	        rd.show();
 	}
+	
+	private void AddFilesBox(){
+		ad = new Dialog(CompareActivity.this);
+        ad.setTitle(getString(R.string.paramsFilesDialogTitle));
+        ad.setContentView(R.layout.dialog_list);
+        Button b1 = (Button) ad.findViewById(R.id.button_validation);
+        Button b2 = (Button) ad.findViewById(R.id.button_cancel);
+
+		//Titres des listes
+		AdaptateurFiles = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+		
+		LvAllCsvFiles = (ListView) ad.findViewById(R.id.lvCSVFiles);	
+		LvAllCsvFiles.setOnItemClickListener(mCSVClickListener);
+		        
+		getListFiles();
+		
+        b1.setOnClickListener(new OnClickListener() {
+	         @Override
+	         public void onClick(View v) {
+	        	 ad.dismiss();  
+	          }    
+        });
+        b2.setOnClickListener(new OnClickListener() {
+	         @Override
+	         public void onClick(View v) {
+	            ad.dismiss(); // dismiss the dialog
+	         }    
+        });
+        
+        ad.show();
+}
+	
 	
 	 public OnItemClickListener mCSVClickListener = new OnItemClickListener() {
 		 @Override
@@ -128,9 +184,18 @@ public class CompareActivity extends Activity {
 						ArrayDataImportFile = new ArrayList<Integer[]>();
 						try {
 							Log.d("TEST", "try");
-							ArrayDataImportFile = ReaderCSVFile(ArrayListCsvFiles.get(i));
+							if(ArrayFilesCharging.size()==0)
+							{
+								ArrayDataImportFile = ReaderCSVFile(ArrayListCsvFiles.get(i));
+								ArrayFilesCharging.add(ArrayListCsvFiles.get(i));
+							}
+							else if(ArrayFilesCharging.size()==1)
+							{
+								ArrayDataImportFile2 = ReaderCSVFile(ArrayListCsvFiles.get(i));
+								ArrayFilesCharging.add(ArrayListCsvFiles.get(i));
+							}
 							dessiner_graph();
-							d.dismiss();
+							ad.dismiss();
 						} catch (IOException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
@@ -141,7 +206,40 @@ public class CompareActivity extends Activity {
 				
 			}
 	 };
-	
+
+	 
+	 public OnItemClickListener mCSVClickRemoveListener = new OnItemClickListener() {
+		 @Override
+			public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3)
+			{			
+				String info = ((TextView) v).getText().toString();
+				String fileName = info.substring(0, 29);
+				Log.v("FILENAME", fileName);
+				
+				Log.d("Nbr CSV",  ArrayFilesCharging.size()+"");
+				
+				for(int i = 0; i < ArrayFilesCharging.size(); i++) {
+					
+					if(ArrayFilesCharging.get(i).getName().equals(fileName)) {						
+						ArrayFilesCharging.remove(ArrayFilesCharging.get(i));
+						if(i==0)
+						{
+							ArrayDataImportFile2=ArrayDataImportFile;
+							ArrayDataImportFile = new ArrayList<Integer[]>();
+						}
+						if(i==1)
+						{
+							ArrayDataImportFile2 = new ArrayList<Integer[]>();
+						}
+						dessiner_graph();
+						rd.dismiss();
+					}
+				}
+				
+			}
+	 };
+	 
+	 
 	 /***lecture du fichier CSV
 	 * @throws IOException ***/
 	 public ArrayList<Integer[]> ReaderCSVFile (File file) throws IOException{
@@ -200,6 +298,8 @@ public class CompareActivity extends Activity {
 	    	
 	    	graphView.addSeries(seriesMeditation);
 	    	graphView.addSeries(seriesAttention);
+		    graphView.addSeries(seriesMeditation2);
+	    	graphView.addSeries(seriesAttention2);
 	    	
 			graphView.setManualYAxisBounds((double) max, (double) min);
 			graphView.setShowLegend(true);
@@ -222,17 +322,31 @@ public class CompareActivity extends Activity {
 		
 	   	graphView.removeSeries(seriesMeditation);
     	graphView.removeSeries(seriesAttention);
-		seriesAttention = new GraphViewSeries("Attention", new GraphViewSeriesStyle(Color.rgb(200, 50, 00), 3), new GraphViewData[] {});
+	   	seriesAttention = new GraphViewSeries("Attention", new GraphViewSeriesStyle(Color.rgb(200, 50, 00), 3), new GraphViewData[] {});
 	    seriesMeditation = new GraphViewSeries("Meditation", new GraphViewSeriesStyle(Color.rgb(0, 50, 200), 3), new GraphViewData[] {});
-	   	graphView.addSeries(seriesMeditation);
+	    graphView.addSeries(seriesMeditation);
     	graphView.addSeries(seriesAttention);
     	
-	    for(i=0;i<ArrayDataImportFile.size();i++)
+    	 for(i=0;i<ArrayDataImportFile.size();i++)
+   		{
+   			seriesAttention.appendData( new GraphViewData(ArrayDataImportFile.get(i)[0], ArrayDataImportFile.get(i)[1]), true);
+   			seriesMeditation.appendData( new GraphViewData(ArrayDataImportFile.get(i)[0], ArrayDataImportFile.get(i)[2]), true);
+   		}
+    	
+	    graphView.removeSeries(seriesMeditation2);
+    	graphView.removeSeries(seriesAttention2);
+	    seriesAttention2 = new GraphViewSeries("Attention2", new GraphViewSeriesStyle(Color.rgb(200, 50, 00), 3), new GraphViewData[] {});
+	    seriesMeditation2 = new GraphViewSeries("Meditation2", new GraphViewSeriesStyle(Color.rgb(0, 50, 200), 3), new GraphViewData[] {});
+	    graphView.addSeries(seriesMeditation2);
+    	graphView.addSeries(seriesAttention2);
+    	
+	 
+	    for(i=0;i<ArrayDataImportFile2.size();i++)
 		{
-			seriesAttention.appendData( new GraphViewData(ArrayDataImportFile.get(i)[0], ArrayDataImportFile.get(i)[1]), true);
-			seriesMeditation.appendData( new GraphViewData(ArrayDataImportFile.get(i)[0], ArrayDataImportFile.get(i)[2]), true);
+			seriesAttention2.appendData( new GraphViewData(ArrayDataImportFile2.get(i)[0], ArrayDataImportFile2.get(i)[1]), true);
+			seriesMeditation2.appendData( new GraphViewData(ArrayDataImportFile2.get(i)[0], ArrayDataImportFile2.get(i)[2]), true);
 		}
-		Log.v("GRAPH", "aval");
+
 	}
 	
 	@Override
@@ -258,8 +372,27 @@ public class CompareActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case R.id.addSave:
-				FilesBox();
+				if(ArrayFilesCharging.size()<2)
+				{
+					AddFilesBox();
+				}
+				else
+				{
+					 Toast.makeText(getApplicationContext(),"Trop de fichiers chargés ! Veuillez en supprimer avant !",Toast.LENGTH_LONG).show();
+				}
+				
 				return true;
+			case R.id.removeSave:
+					if(ArrayFilesCharging.size()>0)
+					{
+						RemoveFilesBox();
+					}
+					else
+					{
+						 Toast.makeText(getApplicationContext(),"Rien à supprimer !",Toast.LENGTH_LONG).show();
+					}
+					
+					return true;
 			default: 
 	    		return super.onOptionsItemSelected(item);
 		}
