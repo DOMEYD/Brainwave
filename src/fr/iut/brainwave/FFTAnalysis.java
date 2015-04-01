a28bddc2608d609737ec8e4a3b734ce93dcde78f
Kasibear <benj-le-kiwi@hotmail.fr>
2015-04-01 07:31:22 +0200

    fft

---
+++ src/fr/iut/brainwave/FFTAnalysis.java
@@ -0,0 +1,20 @@
+package fr.iut.brainwave;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * Permet d'afficher le menu d'aide
+ * 
+ * @author Robin, Chafik, Loïc, Cécile
+ *
+ */
+public class FFTAnalysis extends Activity {
+	@Override
+	
+	
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_aide);
+	}
+}
